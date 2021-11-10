776575271299456406f2fc8f26f1bd1233cebe1e
spauldhaliwal <paul@kangaroorewards.com>
2021-11-10 12:08:50 -0500

    user authentication fix

--- kangaroo_app_sdk/lib/user_authentication/user_authentication_api.dart
+++ kangaroo_app_sdk/lib/user_authentication/user_authentication_api.dart
@@ -1,6 +1,11 @@
 import 'package:kangaroo_app_sdk_platform_interface/platform_interface/base_platform_interface.dart';
 import 'package:kangaroo_app_sdk_platform_interface/platform_interface/features/user_authentication/user_authentication.dart';
 
+export 'package:kangaroo_app_sdk_platform_interface/platform_interface/base_platform_interface.dart'
+    show Result;
+export 'package:kangaroo_app_sdk_platform_interface/platform_interface/features/user_authentication/user_authentication.dart'
+    show UserAuthenticationModel;
+
 class UserAuthenticationApi {
   static authenticateUser(String username, String password) {
     UserAuthenticationApiInterface.instance.authenticateUser(
