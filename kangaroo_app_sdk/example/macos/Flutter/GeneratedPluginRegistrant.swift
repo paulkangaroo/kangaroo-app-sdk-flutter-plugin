e985bba27c68057b978b0daa5c17c0be49ccca82
spauldhaliwal <paul@kangaroorewards.com>
2022-01-28 15:33:23 -0500

    updated freezed pkg

--- kangaroo_app_sdk/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ kangaroo_app_sdk/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import kangaroo_app_sdk_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  kangaroo_app_sdkPlugin.register(with: registry.registrar(forPlugin: "kangaroo_app_sdkPlugin"))
 }
