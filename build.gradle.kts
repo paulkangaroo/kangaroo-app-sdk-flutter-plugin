b41c766c7535ed60ecdbf981bd25e0c2f980e3d5
hassanshaddad <hassan.shaddad@kangaroorewards.com>
2025-06-30 15:30:49 +0300

    Upgraded kangarooApiVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     id("de.undercouch.download") version "4.1.2"
 }
 
-val kangarooApiVersion = "0.0.598"
+val kangarooApiVersion = "0.0.630"
 //flutter packages pub run build_runner build --delete-conflicting-outputs
 
 /**
